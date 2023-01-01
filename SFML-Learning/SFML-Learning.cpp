a82f7126fe78e88769ed95e0699852218ca61594
MypkaXD <thuwashev2016@ayndex.ru>
2023-01-01 20:34:25 +0300

    class Game

--- SFML-Learning/SFML-Learning.cpp
+++ SFML-Learning/SFML-Learning.cpp
@@ -1,26 +1,44 @@
 ﻿#include <SFML/Graphics.hpp>
 
+class Game {
+private:
+    sf::RenderWindow m_window;
+    sf::CircleShape m_player;
+public:
+    Game(size_t width, size_t heigth, char* name) : m_window(sf::VideoMode(width, heigth), name), m_player() {
+        m_player.setFillColor(sf::Color::Cyan);
+        m_player.setRadius(10.f);
+    }
+    void run() {
+        while (m_window.isOpen()) {
+            processEvents();
+            update();
+            render();
+        }
+    }
+private:
+    void update() {
+
+    }
+    void render() {
+        m_window.clear();
+        m_window.draw(m_player);
+        m_window.display();
+    }
+    void processEvents() {
+        sf::Event event;
+        while (m_window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                m_window.close();
+        }
+    }
+};
+
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    //test
+    Game game(1920,1080,"CRINGE");
+    game.run();
 
     return 0;
 }
