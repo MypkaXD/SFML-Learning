46b0687af2c1cd2329245dd65a96a28694efbe1c
MypkaXD <thuwashev2016@ayndex.ru>
2023-01-01 21:22:05 +0300

    added DVIZHENIEXDDD

--- SFML-Learning/SFML-Learning.cpp
+++ SFML-Learning/SFML-Learning.cpp
@@ -1,9 +1,12 @@
 ﻿#include <SFML/Graphics.hpp>
 
+
+
 class Game {
 private:
     sf::RenderWindow m_window;
     sf::CircleShape m_player;
+    bool mIsMovingUp, mIsMovingDown, mIsMovingLeft, mIsMovingRight;
 public:
     Game(size_t width, size_t heigth, char* name) : m_window(sf::VideoMode(width, heigth), name), m_player() {
         m_player.setFillColor(sf::Color::Cyan);
@@ -18,7 +21,16 @@ public:
     }
 private:
     void update() {
-
+        sf::Vector2f movement(0.f, 0.f);
+        if (mIsMovingUp)
+            movement.y -= 0.1f;
+        if (mIsMovingDown)
+            movement.y += 0.1f;
+        if (mIsMovingLeft)
+            movement.x -= 0.1f;
+        if (mIsMovingRight)
+            movement.x += 0.1f;
+        m_player.move(movement);
     }
     void render() {
         m_window.clear();
@@ -28,8 +40,39 @@ private:
     void processEvents() {
         sf::Event event;
         while (m_window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            switch (event.type)
+            {
+            case sf::Event::KeyPressed:// клавиша нажата
+                handlePlayerInput(event.key.code, true);
+                break;
+            case sf::Event::KeyReleased: // клавиша отпущена
+                handlePlayerInput(event.key.code, false);
+                break;
+            case sf::Event::Closed:
                 m_window.close();
+                break;
+            default:
+                break;
+            }
+        }
+    }
+    void handlePlayerInput(sf::Keyboard::Key key, bool isPressed) {
+        switch (key)
+        {
+        case sf::Keyboard::W:
+            mIsMovingUp = isPressed;
+            break;
+        case sf::Keyboard::A:
+            mIsMovingLeft = isPressed;
+            break;
+        case sf::Keyboard::S:
+            mIsMovingDown = isPressed;
+            break;
+        case sf::Keyboard::D:
+            mIsMovingRight = isPressed;
+            break;
+        default:
+            break;
         }
     }
 };
